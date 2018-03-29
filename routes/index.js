c9c9a07f4e225580578e587b32d0e2860ce89acc
kzhang21 <kellyzhang2197@gmail.com>
2018-03-29 14:43:52 -0400

    yelp searchhhh barrr

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,62 @@
 var express = require('express');
-var router = express.Router();
-const request = require("request");
+    router = express.Router();
+    app = express();
 
+const yelp = require('yelp-fusion');
+const api_key = 'mj09D1hJNRjXF_hmstbghBmvy61wjfhp9ACIE5P6SP0qy0QvjUthGt9w2XzvmtzkqIlGc34lKNrrIZlPFutzsmvOmiphaV93bnxh3DTBTJrqvcFxsOOy9g-JI8m2WnYx';
+const client = yelp.client(api_key);
+
+var firstResult;
 
 /* GET home page. */
-router.get('/', (req, res) => {
-    res.sendFile('index.html', {
-    root: './views'
-});
+app.get('/', function(req, res) {
+
+    res.render('index');
 });
 
-router.post('/', function(req,res,next){
-  const options = {
-      method: 'GET',
-      url: 'https://api.yelp.com/v3/businesses/search',
-  };
-    request(options, function (error, response, body){
-      if (error) throw new Error(error);
+app.post('/',function(req,res){
+    var keyword = req.body.term;
+    var loc = req.body.location;
+    var searchRequest = {
+        term: keyword,
+        location: loc
+    }
 
-      console.log(body);
-      res.render('index',{result: JSON.parse(body)});
+    var firstResult = {
+        name: "",
+        rating: "",
+        address: "",
+        phone: ""
+    };
+    console.log(searchRequest);
+    // noinspection JSAnnotator
+    client.search(searchRequest).then(response => {
+        console.log("Here", response);
+
+    r = response.jsonBody.businesses[0];
+
+    console.log(r);
+    firstResult.name = r.name;
+    firstResult.address = r.location.display_address;
+    firstResult.phone = r.display_phone;
+    firstResult.rating = r.rating;
+    const prettyJson = JSON.stringify(firstResult, null, 4);
+
+    res.render('index', {
+        term: searchRequest.term,
+        name: firstResult.name,
+        rating: firstResult.rating,
+        address: firstResult.address,
+        phone: firstResult.phone
+        // address: firstResult.location.display_address,
+        // phone: firstResult.display_phone
     });
-  
+    // console.log(prettyJson);
 });
 
 
-module.exports = router;
+    console.log("First result", firstResult);
+
+});
+
+module.exports = app;
